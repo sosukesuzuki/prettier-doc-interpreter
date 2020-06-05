55b3a51fdc733568eefa02832754438178124a34
Sosuke Suzuki <aosukeke@gmail.com>
2020-06-05 18:22:48 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function foo(arg: string): string {
+  return arg + "HELLO";
+}
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  output: {
+    filename: "index.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+};
